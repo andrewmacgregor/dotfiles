2c424cc186da70efa3ee3c235267e6d88472c206
Andrew Macgregor <andrew.macgregor@agworld.com.au>
2019-12-09 09:50:43 +0800

    Further setup steps

--- setup_steps.sh
+++ setup_steps.sh
@@ -1,3 +1,5 @@
+brew upgrade openssl
+brew install nvm
 brew tap homebrew/cask-fonts && brew cask install font-source-code-pro
 brew install rbenv
 rbenv install 2.3.1
@@ -5,6 +7,7 @@ rbenv install 2.6.5
 brew install nvm # follow instructions
 brew cask install postgres
 sudo mkdir -p /etc/paths.d && echo /Applications/Postgres.app/Contents/Versions/latest/bin | sudo tee /etc/paths.d/postgresapp
+brew cask install karabiner-elements
 brew cask install docker
 brew cask install istat-menus
 brew cask install ccmenu
@@ -14,3 +17,6 @@ brew cask install vagrant-manager
 brew cask install dashlane
 brew cask install whatsapp
 brew install libxml2 libxslt
+brew cask install istat-menus
+brew cask install ccmenu
+brew cask install carbon-copy-cloner
